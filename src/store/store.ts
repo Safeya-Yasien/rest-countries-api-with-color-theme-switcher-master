7c85e292e97b549c1ba8c3653e7c2aa92d2da4a4
Safeya-Yasien <ssafeya5@gmail.com>
2025-05-19 17:00:09 +0300

    solve serializable value

--- src/store/store.ts
+++ src/store/store.ts
@@ -22,6 +22,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
